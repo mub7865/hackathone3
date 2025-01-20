83ebaf0883f173ecfdcb70f586fe96397cd8cec3
Muhammad Ubaid Ansari <muhammadubaidansari145@gmail.com>
2025-01-21 01:06:58 +0500

    updated  commit

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import CategoryContent from "@/components/CategoryContent";
 
 
@@ -5,8 +6,8 @@ export default function CategoriesPage() {
 
 
   return (
-    <>
+    <Suspense fallback={<div>Loading categories...</div>}>
     <CategoryContent/>
-    </>
+    </Suspense>
   );
 }
